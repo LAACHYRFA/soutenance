f4f495a1a385e972d4a872bb50048ab105a83322
laachyr farah <laachyr.farah.solicode@gmail.com>
2026-06-12 12:08:08 +0100

    Delete pfe_gym directory

--- pfe_gym/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start(); // ضروري باش نوصلو للـ session
-session_unset(); // كتمسح كاع الـ variables ديال الـ session
-session_destroy(); // كتدمر الـ session كاملة
-header("Location: index.php"); // كترجع المستخدم للواجهة
-exit();
-?>
